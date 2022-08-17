c4b0ce82b29a5cd27c6304e4bdc8f151c5047209
HSS <2215672221@qq.com>
2022-08-17 19:09:02 +0800

    修复抽奖

--- scripts/npc/2042002_1.js
+++ scripts/npc/2042002_1.js
@@ -1,54 +1,92 @@
-var status = -1;
-var sel;
+/*
+SnailMS脚本生成器
+*/
+
+var target1 = 0;
 
 function start() {
-    action(1, 0, 0);
+	status = -1;
+	action(1, 0, 0);
 }
 
 function action(mode, type, selection) {
-    if (mode == 0 && status == 0) {
-        cm.dispose();
-        return;
-    }
-    if (mode == 1)
-        status++;
-    else
-        status--;
-
-    if (status == 0)
-        cm.sendSimple("  Hi~#b#h ##k，你要创建一个家族吗？个人的力量始终是弱小的，只有一群人团结起来，才能变得强大。\r\n\r\n#b#L0#创建家族#l\r\n#L1#解散家族#l\r\n#L2#扩充家族#l#k");
-    else if (status == 1) {
-        sel = selection;
-        if (selection == 0) {
-            if (cm.getPlayerStat("GID") > 0) {
-                cm.sendOk("你不能创建一个新的工会.");
-                cm.dispose();
-            } else {
-                cm.sendYesNo("开始抒写一段传奇吧。");
-            }
-        } else if (selection == 1) {
-            if (cm.getPlayerStat("GID") <= 0 || cm.getPlayerStat("GRANK") != 1) {
-                cm.sendOk("你不是公会会长所以不能解散公会");
-                cm.dispose();
-            } else
-                cm.sendYesNo("你确定要解散你的公会?你将无法恢复并且GP消失.");
-        } else if (selection == 2) {
-            if (cm.getPlayerStat("GID") <= 0 || cm.getPlayerStat("GRANK") != 1) {
-                cm.sendOk("你不是公会会长所以不能扩充人数");
-                cm.dispose();
-            } else
-                cm.sendYesNo("扩充公会人数 #b5#k 要 #b2500000 金币#k, 你确定要扩充吗?");
-        }
-    } else if (status == 2) {
-        if (sel == 0 && cm.getPlayerStat("GID") <= 0) {
-            cm.genericGuildMessage(1);
-            cm.dispose();
-        } else if (sel == 1 && cm.getPlayerStat("GID") > 0 && cm.getPlayerStat("GRANK") == 1) {
-            cm.disbandGuild();
-            cm.dispose();
-        } else if (sel == 2 && cm.getPlayerStat("GID") > 0 && cm.getPlayerStat("GRANK") == 1) {
-            cm.increaseGuildCapacity();
-            cm.dispose();
-        }
-    }
+	if (mode == 1) {
+		status++;
+	} else {
+		if (status == 0) {
+			cm.sendOk("对话结束语");
+			cm.dispose();
+			return;
+		}
+		status--;
+	}
+	if (status == 0) {
+		//在这里编写脚本第一步要做的事
+		var text = "#r在#b外面#r，我是怪物嘉年华，在#b自由市场#r，我就是活动总管\r\n#k请选择你要参加的活动：\r\n\r\n";
+		//text += "#L1##bOX答题#k#l\t\t#L2##b打椰子#k#l\t\t#L3##b推雪球#k#l\r\n\r\n";
+		//text += "#L4##b寻宝  #k#l\t\t#L5##b向高地#k#l\t\t#L6##b上楼上楼#k#l\r\n\r\n";
+		cm.sendSimple(text);
+	} else if (status == 1) {
+		//在这里编写第二步要做的事
+		if(selection == 1){
+			//在这里编写选项1要做的事
+			cm.sendYesNo("要把你传送到#rOX答题的比赛场地#k么？")
+			target1 = 1;
+		} else if (selection == 2) {
+			//在这里编写选项2要做的事
+			cm.sendYesNo("要把你传送到#r打椰子的比赛场地#k么？")
+			target1 = 2;
+		} else if (selection == 3) {
+			//在这里编写选项3要做的事
+			cm.sendYesNo("要把你传送到#r推雪球的比赛场地#k么？")
+			target1 = 3;
+		} else if (selection == 4) {
+			//在这里编写选项4要做的事
+			cm.sendYesNo("要把你传送到#r寻宝的比赛场地#k么？")
+			target1 = 4;
+		} else if (selection == 5) {
+			//在这里编写选项5要做的事
+			cm.sendYesNo("要把你传送到#r向高地的比赛场地#k么？")
+			target1 = 5;
+		} else if (selection == 6) {
+			//在这里编写选项6要做的事
+			cm.sendYesNo("要把你传送到#r上楼上楼的比赛场地#k么？")
+			target1 = 6;
+		} 
+		
+	} else if (status == 2){
+		switch(target1){
+			case 1:
+			cm.warp(109020001);
+			break;
+			case 2:
+			cm.warp(109080000);
+			break;
+			case 3:
+			var cs = cm.getPlayer().getClient().getChannelServer();
+			var myEvent =  cs.getEvent(Packages.server.events.MapleEventType.推雪球比赛);
+			if(myEvent.isRunning()){
+				cm.sendOk("抱歉，活动已开始，通道已关闭，下次早点来吧~");
+				cm.dispose();
+				return;
+			}else{
+				cm.warp(109060000);
+			}
+			break;
+			case 4:
+			cm.warp(109010000);
+			break;
+			case 5:
+			cm.warp(109040000);
+			break;
+			case 6:
+			cm.warp(109030001);
+			break;
+		}
+		cm.dispose();
+	} else {
+		cm.dispose();
+		return;
+	}
 }
+
